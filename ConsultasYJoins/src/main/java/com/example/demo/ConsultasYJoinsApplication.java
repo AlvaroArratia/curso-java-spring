0935b891ac95048c2ee091b1327e4b9a7c62b25c
Alvaro Arratia Ramirez <alvaroarratia.r@gmail.com>
2021-07-21 20:20:33 -0400

    consultas y joins

---
+++ ConsultasYJoins/src/main/java/com/example/demo/ConsultasYJoinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultasYJoinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultasYJoinsApplication.class, args);
+	}
+
+}
