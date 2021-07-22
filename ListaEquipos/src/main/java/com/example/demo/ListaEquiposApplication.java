edf81cb32d9237a56a764b50374b4e26cbaf2eb4
Alvaro Arratia Ramirez <alvaroarratia.r@gmail.com>
2021-07-21 20:18:03 -0400

    lista de equipos

---
+++ ListaEquipos/src/main/java/com/example/demo/ListaEquiposApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaEquiposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaEquiposApplication.class, args);
+	}
+
+}
