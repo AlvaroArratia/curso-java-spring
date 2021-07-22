3e558164f3a94370afbe1306399b81f3e7729b94
Alvaro Arratia Ramirez <alvaroarratia.r@gmail.com>
2021-07-21 20:34:15 -0400

    repaso examen

---
+++ RepasoExamen1/src/test/java/com/example/demo/RepasoExamen1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepasoExamen1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
