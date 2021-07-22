08280dfdfa85375337c1aa393eaee3d71b937d73
Alvaro Arratia Ramirez <alvaroarratia.r@gmail.com>
2021-07-21 20:32:37 -0400

    controladores y vistas

---
+++ ControladoresYVistas/src/main/java/com/example/demo/ControladoresYVistasApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControladoresYVistasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControladoresYVistasApplication.class, args);
+	}
+
+}
